0137fec826674089dae8f08f3c60a8ea47bb4b5f
cr7peyo <166892248+cr7peyo@users.noreply.github.com>
2024-05-11 12:44:13 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+        // script.js
+        function crearEstrellaFugaz() {
+            const estrella = document.createElement("div");
+            estrella.className = "estrella-fugaz";
+            estrella.style.left = Math.random() * 100 + "vw";
+            estrella.style.animationDuration = Math.random() * 2 + 1 + "s";
+            document.querySelector(".lluvia").appendChild(estrella);
+        
+            setTimeout(() => {
+                estrella.remove();
+            }, 2000);
+        }
+
+        setInterval(crearEstrellaFugaz, 200);
+
+        document.addEventListener("DOMContentLoaded", function() {
+            document.body.style.display = "none";
+            setTimeout(function() {
+              document.body.style.display = "block";
+            }, 4000);
+});
